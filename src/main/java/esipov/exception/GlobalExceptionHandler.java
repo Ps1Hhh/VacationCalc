b1da443a321f39619f77616f0da20925a8aba850
Ps1Hhh <sergeyesipov7@gmail.com>
2024-03-31 12:16:39 +0400

    add ControllerAdvice

---
+++ src/main/java/esipov/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package esipov.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IncorrectDataVacationException.class)
+    public ResponseEntity<String> handleIncorrectDataVacationException(IncorrectDataVacationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
+
+
